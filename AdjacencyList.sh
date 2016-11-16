81664d09d24065a855cc09c3a3e594097c0ae1d3
Yuki Ishikawa <7023.yu.ki@gmail.com>
2016-11-16 12:47:08 +0900

    finish field-selectable AdjacencyList

--- AdjacencyList.sh
+++ AdjacencyList.sh
@@ -17,70 +17,70 @@ cd ..
 
 if [ $1 == "SA" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $1}' > x
-    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $1}' > Header
+    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $1}' > c
 fi
 
 if [ $1 == "DA" ]  ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $2}' > x
-    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $2}' > Header
+    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $2}' > c
 elif [ $2 == "DA" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $2}' > y
     paste x y > z
     mv z x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $2}' > a
-    paste Header a > b
-    mv b Header
+    paste c a > b
+    mv b c
 fi
 
 if [ $1 == "SP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $3}' > x
-    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $3}' > Header
+    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $3}' > c
 elif [  $2 == "SP" ] || [ $3 == "SP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $3}' > y
     paste x y > z
     mv z x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $3}' > a
-    paste Header a > b
-    mv b Header
+    paste c a > b
+    mv b c
 fi
 
 if [ $1 == "DP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $4}' > x
-    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $4}' > Header
+    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $4}' > c
 elif [ $2 == "DP" ] || [ $3 == "DP" ] || [ $4 == "DP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $4}' > y
     paste x y > z
     mv z x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $4}' > a
-    paste Header a > b
-    mv b Header
+    paste c a > b
+    mv b c
 fi
 
 if [ $1 == "PROT" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $5}' > x
-    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $5}' > Header
+    cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $5}' > c
     if [ $2 == "FLAG" ] ; then
 	cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $6}' > y
 	paste x y > z
 	mv z x
 	cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $6}' > a
-	paste Header a > b
-	mv b Header
+	paste c a > b
+	mv b c
     fi 
 elif [ $2 == "PROT" ] || [ $3 == "PROT" ] || [ $4 == "PROT" ] || [ $5 == "PROT" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $5}' > y
     paste x y > z
     mv z x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $5}' > a
-    paste Header a > b
-    mv b Header
+    paste c a > b
+    mv b c
     if [ $3 == "FLAG" ] || [ $4 == "FLAG" ] || [ $5 == "FLAG" ] || [ $6 == "FLAG" ] ; then
 	cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $6}' > y
 	paste x y > z
 	mv z x
 	cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $6}' > a
-	paste Header a > b
-	mv b Header
+	paste c a > b
+	mv b c
     fi    
 elif [ $1 == "FLAG" ] || [ $2 == "FLAG" ] || [ $3 == "FLAG" ] || [ $4 == "FLAG" ] || [ $5 == "FLAG" ] ; then
     echo "error : FLAG needs PROT argument."
@@ -90,24 +90,24 @@ fi
 #echo "input probability of evaluation type (0 <= p <=1)"
 # read PROBABILITY
 javac AddEtype.java
-eval  java AddEtype x OriginalList '$'{$pro}
+eval  java AddEtype x w '$'{$pro}
 javac ClassBenchToAdjacencyList.java
 case "$#" in
-    "6") eval java ClassBenchToAdjacencyList OriginalList Header '$'{$ruleName} $1
+    "6") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1
 	 ;;
-    "7") eval java ClassBenchToAdjacencyList OriginalList Header '$'{$ruleName} $1 $2
+    "7") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1 $2
 	 ;;
-    "8") eval java ClassBenchToAdjacencyList OriginalList Header '$'{$ruleName} $1 $2 $3
+    "8") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1 $2 $3
 	 ;;
-    "9") eval java ClassBenchToAdjacencyList OriginalList Header '$'{$ruleName} $1 $2 $3 $4
+    "9") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1 $2 $3 $4
 	 ;;
-    "10") eval java ClassBenchToAdjacencyList OriginalList Header '$'{$ruleName} $1 $2 $3 $4 $5
+    "10") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1 $2 $3 $4 $5
 	  ;;
-    "11") eval java ClassBenchToAdjacencyList OriginalList Header '$'{$ruleName} $1 $2 $3 $4 $5 $6
+    "11") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1 $2 $3 $4 $5 $6
 esac
 
 rm a
 rm x
 rm y
-rm Header
-rm OriginalList
+rm c
+rm w
