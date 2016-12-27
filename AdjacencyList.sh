89ae7eba23c464cbf04e115f13ba5a66f09cfce3
Yuki Ishikawa <7023.yu.ki@gmail.com>
2016-12-27 16:06:23 +0900

    remove defaultRule

--- AdjacencyList.sh
+++ AdjacencyList.sh
@@ -3,6 +3,7 @@ seed_file=`expr $# - 3`
 headNum=`expr $# - 2`
 ruleName=`expr $# - 1`
 pro=$#
+defaultRule=""
 
 cd db_generator
 eval ./db_generator -bc ../parameter_files/'$'$seed_file '$'$ruleNum 2 0.5 -0.1 MyFilters
@@ -14,11 +15,13 @@ cd ..
 if [ $1 == "SA" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $1}' > x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $1}' > c
+    defaultRule="${defaultRule}s;@0.0.0.0/0"
 fi
 
 if [ $1 == "DA" ]  ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $2}' > x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $2}' > c
+    defaultRule="${defaultRule}s;0.0.0.0/0"
 elif [ $2 == "DA" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $2}' > y
     paste x y > z
@@ -26,11 +29,13 @@ elif [ $2 == "DA" ] ; then
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $2}' > a
     paste c a > b
     mv b c
+    defaultRule="${defaultRule}	0.0.0.0/0"
 fi
 
 if [ $1 == "SP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $3}' > x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $3}' > c
+    defaultRule="${defaultRule}s;0 : 65535"
 elif [  $2 == "SP" ] || [ $3 == "SP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $3}' > y
     paste x y > z
@@ -38,11 +43,13 @@ elif [  $2 == "SP" ] || [ $3 == "SP" ] ; then
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $3}' > a
     paste c a > b
     mv b c
+    defaultRule="${defaultRule}	0 : 65535"
 fi
 
 if [ $1 == "DP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $4}' > x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $4}' > c
+    defaultRule="${defaultRule}s;0 : 65535"
 elif [ $2 == "DP" ] || [ $3 == "DP" ] || [ $4 == "DP" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $4}' > y
     paste x y > z
@@ -50,11 +57,13 @@ elif [ $2 == "DP" ] || [ $3 == "DP" ] || [ $4 == "DP" ] ; then
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $4}' > a
     paste c a > b
     mv b c
+    defaultRule="${defaultRule}	0 : 65535"
 fi
 
 if [ $1 == "PROT" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $5}' > x
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $5}' > c
+    defaultRule="${defaultRule}s;0x00/0x00"
     if [ $2 == "FLAG" ] ; then
 	cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $6}' > y
 	paste x y > z
@@ -62,6 +71,7 @@ if [ $1 == "PROT" ] ; then
 	cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $6}' > a
 	paste c a > b
 	mv b c
+	defaultRule="${defaultRule}	0x0000/0x0000"
     fi 
 elif [ $2 == "PROT" ] || [ $3 == "PROT" ] || [ $4 == "PROT" ] || [ $5 == "PROT" ] ; then
     cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $5}' > y
@@ -70,6 +80,7 @@ elif [ $2 == "PROT" ] || [ $3 == "PROT" ] || [ $4 == "PROT" ] || [ $5 == "PROT" 
     cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $5}' > a
     paste c a > b
     mv b c
+    defaultRule="${defaultRule}	0x00/0x00"
     if [ $3 == "FLAG" ] || [ $4 == "FLAG" ] || [ $5 == "FLAG" ] || [ $6 == "FLAG" ] ; then
 	cat db_generator/MyFilters | awk -F'\t' 'BEGIN{OFS="\t"} {print $6}' > y
 	paste x y > z
@@ -77,13 +88,20 @@ elif [ $2 == "PROT" ] || [ $3 == "PROT" ] || [ $4 == "PROT" ] || [ $5 == "PROT" 
 	cat db_generator/MyFilters_trace | awk 'BEGIN{OFS="\t"} {print $6}' > a
 	paste c a > b
 	mv b c
+	defaultRule="${defaultRule}	0x0000/0x0000"
     fi    
 elif [ $1 == "FLAG" ] || [ $2 == "FLAG" ] || [ $3 == "FLAG" ] || [ $4 == "FLAG" ] || [ $5 == "FLAG" ] ; then
     echo "error : FLAG needs PROT argument."
     exit
 fi
 
-eval  java AddEtype x w '$'{$pro}
+#echo " \"$defaultRule++g\" "
+sed -e "$defaultRule;;g" < x > b #デフォルトルール消去
+sed '/^$/d' b > e #空行消去
+#awk '!Overlap[$0]++' x > d #重複消去
+
+
+eval  java AddEtype e w '$'{$pro}
 case "$#" in
     "6") eval java ClassBenchToAdjacencyList w c '$'{$ruleName} $1
 	 ;;
@@ -99,8 +117,8 @@ case "$#" in
 	  ;;
 esac
 
-
-rm x
+#rm e
+#rm x
 rm c
 if [ ! $# == 6 ] ; then
     rm a
